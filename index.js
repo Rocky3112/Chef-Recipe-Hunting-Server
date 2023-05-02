6eb298a449d03841bb88df66640ec6ff79013299
Rocky3112 <alomgirhossainrocky@gmail.com>
2023-05-02 12:23:12 +0600

    server side has been created

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+const categories = require('./data/data.json');
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Server is running')
+
+});
+
+app.listen(port, () => {
+    console.log(`API is running on port: ${port}`)
+})
